7426c175ad6df25f69787ac0bb08587f02bc1d93
Maki Aboabida <maki-miko@hotmail.com>
2021-08-18 20:46:02 -0400

    added punch touch

--- assets/js/script.js
+++ assets/js/script.js
@@ -163,7 +163,19 @@ saveTasks();
 });
 
 
-$("#trash").droppable()
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 
 loadTasks();
 
